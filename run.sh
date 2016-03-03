05afc14463889ed8751a92838f9771c847b6696b
MartinMikita <martin.mikita@klokantech.com>
2016-03-03 14:11:30 +0100

    Changed run script to tag and run devel (instead of latest).

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ cp sample.tsv data/
 
 docker stop osmnames-sphinxsearch
 docker rm osmnames-sphinxsearch
-docker build -t klokantech/osmnames-sphinxsearch .
+docker build -t klokantech/osmnames-sphinxsearch:devel .
 docker run -d --name osmnames-sphinxsearch \
     -p 9001:80 \
     -v `pwd`/data/:/data/ \
     -v `pwd`/tmp/:/tmp/ \
-    klokantech/osmnames-sphinxsearch
+    klokantech/osmnames-sphinxsearch:devel
